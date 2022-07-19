a545bf95e1e2688788960b5df912f94c94d046f2
Andrei <andreisva2023@gmail.com>
2022-07-18 23:52:17 -0700

    add cli option infrastructure

--- src/main.c
+++ src/main.c
@@ -1,5 +1,28 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+static void usage() {
+	fprintf(stderr, "usage: trix [-hv]\n");
+}
 
 int main(int argc, char** argv) {
+	if (argc == 1) {
+		usage();
+		return 0;
+	}
+
+	int ch;
+	while ((ch = getopt(argc, argv, "hv")) != -1) {
+		switch (ch) {
+		case 'h':
+			usage();
+			break;
+		case 'v':
+			break;
+		default:
+			usage();
+		}
+	}
 	return 0;
 }
